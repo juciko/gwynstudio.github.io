91a7bbb53353d6e3e2136e48eb08840415558e0f
andreanus <andreanusciko@gmail.com>
2023-10-21 17:00:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik di luar nav
+const hamburger = document.querySelector
+('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
